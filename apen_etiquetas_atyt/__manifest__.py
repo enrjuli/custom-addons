9ef089a6c1ba82e10f89f60109dd43e48c1eaa4a
enrjuli <ejulia@apen.es>
2022-10-28 06:58:31 +0000

    UPDTE ETIQUETES

--- apen_etiquetas_atyt/__manifest__.py
+++ apen_etiquetas_atyt/__manifest__.py
@@ -1,11 +1,12 @@
 {
     'name':'Etiquetas ZPL Personalizadas ',
-    'summary': "Personalizacion de etiquetas para la impresion en ZPL",
+    'summary': "Personalizacion de etiquetas para la impresion en ZPL ",
     'description': "Personalizacion de etiquetas para la impresion en ZPL", 
     'author': "Apen", 
     'website': "http://www.apen.es/", 
     'category': 'Reports', 
-    'version': '14.0.1.0.1', 
+    'version': '14.0.1.0.1',
+    "license": "AGPL-3", 
     'depends': ['base','stock','mrp','sale'],
     'data': [
         "reports/custom_report.xml",
@@ -13,7 +14,7 @@
         "reports/etiqueta_1_template_view.xml", 
         "reports/etiqueta_3_production_template_view.xml",  
         "reports/etiqueta_trasferencias_view.xml",
+        "reports/etiqueta_lote_template.xml",
         'views/mrp_production_form_inherit.xml',
     ], 
-    "license": "AGPL-3",
  }
--- apen_etiquetas_atyt/models/__init__.py
+++ apen_etiquetas_atyt/models/__init__.py
@@ -1,2 +1,2 @@
 from . import mrp_production
-from . import stock_picking
+from . import stock
